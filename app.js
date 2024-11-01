049d1b6efc8ca2e472383dc9dd86028171cbca56
patelsweta2 <sweta0442@gmail.com>
2024-11-01 14:10:50 +0530

    change cors policy

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ const { tweetRouter } = require("./backend/routes/tweets.routes");
 
 const app = express();
 
-const ALLOWED_URLS = [
-  "https://patelsweta2.github.io/Twittter-MERN-STACK-FrontEnd/",
-];
-console.log(ALLOWED_URLS);
+// const ALLOWED_URLS = [
+//   "https://patelsweta2.github.io/Twittter-MERN-STACK-FrontEnd/",
+// ];
+// console.log(ALLOWED_URLS);
 
-const corsOptions = {
-  origin: ALLOWED_URLS,
-  credentials: true,
-  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  exposedHeaders: ["Access-Control-Allow-Private-Network"],
-};
+// const corsOptions = {
+//   origin: ALLOWED_URLS,
+//   credentials: true,
+//   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   exposedHeaders: ["Access-Control-Allow-Private-Network"],
+// };
 
-app.use(cors(corsOptions));
-app.options("*", cors());
+app.use(cors());
+// app.options("*", cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
